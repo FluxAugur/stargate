f2fa2cb319935ba4d60c2cdfba67df9840d61d9c
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-10-10 05:47:56 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stargate::Application.config.secret_key_base = '9a247684a6e1944b2e4a3124aaba9e94f9c7dd9db1c1d2c489e1a0e14704feefc01fc428d288c6dcd676c017a669a22fe25f81e08e55d488b1240176e19e210d'
