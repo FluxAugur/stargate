c952b1e5743a66090ced4a31afb11897c86bd88c
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-10-10 09:41:54 -0400

    adding action mailer config (fixing typo)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Stargate::Application.configure do
   config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset = "utf-8"
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
